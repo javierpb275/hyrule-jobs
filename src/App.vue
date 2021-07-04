d585223251e874342b5420cc9b782731d75794fe
javierpb275 <javierpb275@gmail.com>
2021-07-04 17:35:21 +0200

    created type orderterm. used orderterm for variable order and handleclik function to change the value of order to some of the options of ordertem.
    passed order to jobslist component as a prop and show value in template.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
 <div class="app">
-  <JobsList :jobs="jobs"/>
+   <header>
+     <div class="order">
+      <button @click="handleClick('title')">order by title</button>
+      <button @click="handleClick('salary')">order by salary</button>
+      <button @click="handleClick('location')">order by location</button>
+     </div>
+   </header>
+  <JobsList :jobs="jobs" :order="order"/>
 </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import Job from './types/Job'
+import Job from './types/Job';
+import OrderTerm from './types/OrderTerm';
 import JobsList from './components/JobsList.vue';
 
 export default defineComponent({
@@ -20,13 +28,34 @@ export default defineComponent({
       {title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4'},
       {title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5'},
     ]);
+    const order = ref<OrderTerm>('title');
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    }
     return {
-      jobs
+      jobs,
+      handleClick,
+      order
     }
   }
 });
 </script>
 
 <style>
-
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
 </style>
--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="job-list">
+        <p>Ordered by {{order}}</p>
         <ul>
             <li v-for="job in jobs" :key="job.id">
                 <h2>{{job.title}} in {{job.location}}</h2>
@@ -19,11 +20,16 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import Job from '../types/Job'
+import OrderTerm from '../types/OrderTerm'
 
 export default defineComponent({
     props: {
         jobs: {
             type: Array as PropType<Job[]>,
+            required: true
+        },
+        order: {
+            type: String as PropType<OrderTerm>,
             required: true
         }
     }
