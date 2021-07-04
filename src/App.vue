b29a3d2436328e26c10f1484ee0c621ad9c01c6d
javierpb275 <javierpb275@gmail.com>
2021-07-04 16:17:30 +0200

    composition API: created interface Job. imported Job interface in App.js a created array of jobs using ref and displaying a job location in template.

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,25 @@
 <template>
 <div class="app">
-  <p>{{name}} - {{age}}</p>
-  <button @click="changeName('zelda')">change name</button>
-  <button @click="changeAge(30)">change age</button>
+  <p>{{jobs[0].location}}</p>
 </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, toRefs } from 'vue';
+import { defineComponent, ref } from 'vue';
+import Job from './types/Job'
 
 export default defineComponent({
   name: 'App',
   setup() {
-/*     const state = reactive({
-      name: 'Link',
-      age: 25 as string|number
-    })
-    state.name = 'shaun'
-    state.age = 26
+    const jobs = ref<Job[]>([
+      {title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1'},
+      {title: 'quarryman', location: 'death mountain', salary: 40000, id: '2'},
+      {title: 'flute player', location: 'the lost woods', salary: 35000, id: '3'},
+      {title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4'},
+      {title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5'},
+    ]);
     return {
-      ...toRefs(state)
-    } */
-    const name = ref<string>('Link');
-    const age = ref<string|number>(25)
-    return {
-      name,
-      age
-    }
-  },
-  methods: {
-    changeName(name: string) {
-      this.name = name;
-      return name;
-    },
-    changeAge(age: string|number) {
-      this.age = age;
-      return age;
+      jobs
     }
   }
 });
