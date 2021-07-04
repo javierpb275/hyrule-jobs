5b634b9172c641345715bca6b9593c59a3afd182
javierpb275 <javierpb275@gmail.com>
2021-07-04 17:51:20 +0200

    used computed to create orderedJobs that return the jobs orderd by the value of the prop order

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -2,7 +2,7 @@
     <div class="job-list">
         <p>Ordered by {{order}}</p>
         <ul>
-            <li v-for="job in jobs" :key="job.id">
+            <li v-for="job in orderedJobs" :key="job.id">
                 <h2>{{job.title}} in {{job.location}}</h2>
                 <div class="salary">
                     <p>{{job.salary}} rupees</p>
@@ -18,7 +18,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 import Job from '../types/Job'
 import OrderTerm from '../types/OrderTerm'
 
@@ -31,6 +31,16 @@ export default defineComponent({
         order: {
             type: String as PropType<OrderTerm>,
             required: true
+        }
+    },
+    setup(props) {
+        const orderedJobs = computed(() => {
+            return [...props.jobs].sort((a: Job, b: Job) => {
+                return a[props.order] > b[props.order] ? 1 : -1;
+            })
+        })
+        return {
+            orderedJobs
         }
     }
 })
